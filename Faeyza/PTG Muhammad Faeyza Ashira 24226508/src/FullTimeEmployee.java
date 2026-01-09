d23cbb3c0a9dc8aa7196ef8134c36037e38434b0
ejafee <ashiraeja@gmail.com>
2026-01-09 23:56:56 +0800

    final

--- Faeyza/PTG Muhammad Faeyza Ashira 24226508/src/FullTimeEmployee.java
+++ Faeyza/PTG Muhammad Faeyza Ashira 24226508/src/FullTimeEmployee.java
@@ -1,12 +1,11 @@
 public class FullTimeEmployee extends Employee {
-    // 3 Unique Attributes
+
     private double monthlySalary;
     private double bonus;
     private String healthPlan; // Added to ensure we have 3 attributes
 
-    // Constructor matches Parent + adds new data
     public FullTimeEmployee(String name, int id, String department, double salary, double bonus, String healthPlan) {
-        super(name, id, department); // Passes basic info up to the Employee constructor
+        super(name, id, department);
         this.monthlySalary = salary;
         this.bonus = bonus;
         this.healthPlan = healthPlan;
--- Faeyza/PTG Muhammad Faeyza Ashira 24226508/src/PayrollSystem.java
+++ Faeyza/PTG Muhammad Faeyza Ashira 24226508/src/PayrollSystem.java
@@ -24,15 +24,14 @@ public class PayrollSystem {
 
         //obj
         FullTimeEmployee ft1 = new FullTimeEmployee("eza", 101, "IT", 5000, 1000, "Premium");
-        PartTimeEmployee pt1 = new PartTimeEmployee("aul", 102, "HR", 20, 100, "Morning");
+        PartTimeEmployee pt1 = new PartTimeEmployee("adam", 102, "HR", 20, 100, "Morning");
         PartTimeEmployee pt2 = new PartTimeEmployee("yuda", 103, "Marketing", 25, 80, "Night");
-        PartTimeEmployee pt3 = new PartTimeEmployee("falih", 104, "yes", 20, 95, "Morning");
 
         //lists
         employeeList.add(ft1);
         employeeList.add(pt1);
         employeeList.add(pt2);
-        employeeList.add(pt3);
+
         return employeeList;
     }
 }
